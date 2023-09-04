c66860f8fb2550c2b1299746dc5e067a39049bfe
Stepha-Nek <Nneka.oguh1@gmail.com>
2023-09-03 21:43:17 -0700

    4

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-# Check if a URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Define the POST parameters
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# Send the POST request using curl
-curl -s -X POST "$1" -d "email=$email" -d "subject=$subject"
-
-echo ""
 
